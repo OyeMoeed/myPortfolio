cfd04a21177c8e2da7a198ea87c9bb89e122d7e0
Moeed Sarwar <oyemoeed@gmail.com>
2023-09-10 08:13:20 -0700

    Added Servies

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="max-w-screen max-h-screen px-4 mx-auto font-all overflow-hidden">
-      <div className="flex flex-col border rounded-3xl border-[#383838] justify-center text-justify ">
+    <div className="max-w-screen h-screen px-4 mx-auto font-all overflow-hidden">
+      <div className="flex flex-col  rounded-3xl border border-[#383838] justify-center text-justify ">
         <div className="flex flex-col h-screen justify-between">
           <div className="flex flex-col text-center ">
             <h1 className="text-[#f1f1f1] font-light md:text-[9em] text-[5em]">
@@ -13,7 +13,7 @@ const Hero = () => {
               Sarwar
             </h1>
           </div>
-          <div className="text-[#797979] text-center p-5">
+          <div className="text-[#797979] text-center p-4">
             <p>
               'Your Time Is Limited, So Don't Waste It Living Someone Else's
               Life' - Steve Jobs
---
+++ src/pages/Home/Services.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div className="max-w-screen mx-auto px-4 font-all mt-[9em]">
+      <div className="flex flex-col">
+        <div className="flex justify-center w-screen">
+          {" "}
+          <h1 className="text-[#f1f1f1] pb-[3em]">(MAIN SERVICES)</h1>
+        </div>
+        <div className="flex flex-row justify-between text-[2em] lg:text-[5em] border p-3 rounded-xl border-[#383838]">
+          <div className="text-[#f1f1f1]">Front End</div>
+          <div className="text-[#797979]">Back End</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
