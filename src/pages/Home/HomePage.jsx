3b1b6c9d7e5ec90fa42b4a82f4cda25fdfd0e16a
Moeed Sarwar <oyemoeed@gmail.com>
2023-09-10 08:46:56 -0700

    added speciality

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import Hero from "./Hero";
 import Services from "./Services";
+import Speciality from "./Speciality";
 const HomePage = () => {
   return (
     <div>
       <Hero />
       <Services />
+      <Speciality />
     </div>
   );
 };
---
+++ src/pages/Home/Speciality.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Speciality = () => {
+  return (
+    <div className="max-w-screen max-auto my-[12em] lg:px-[9em] px-[4em] font-all">
+      <div>
+        <h1 className="text-[#797979] text-[1.6em] lg:text-[4em] lg:text-end">
+          I craft digital experiences that balance{" "}
+        </h1>
+        <span className="text-[#f1f1f1] text-[1.6em] lg:text-[3.8em] ">
+          efficiency, aesthetics{" "}
+        </span>
+        <span className="text-[#797979] text-[1.6em] lg:text-[3.8em]">
+          and {"  "}
+        </span>
+        <span className="text-[#f1f1f1] text-[1.6em] lg:text-[3.8em] ">
+          functionality.
+        </span>{" "}
+        <span className="text-[#797979] text-[1.6em] lg:text-[3.8em] ">
+          {" "}
+          Imbuing them with a sense of purpose and character.
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Speciality;
