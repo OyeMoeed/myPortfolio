a7ca10cc655aaafcffdb5f6d351602d556eb00c6
Moeed Sarwar <oyemoeed@gmail.com>
2023-09-11 12:24:06 -0700

    Update Speciality.jsx

--- src/pages/Home/Speciality.jsx
+++ src/pages/Home/Speciality.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Speciality = () => {
   return (
-    <div className="max-w-screen mx-auto px-[3.5em] font-all my-[10em] ">
+    <div className="max-w-screen mx-auto lg:px-[3.5em] px-[2em]  font-all my-[10em] ">
       <div>
         <h1 className="text-[#797979] lg:text-[3rem] md:text-[2.5rem] text-[1rem]">
           <span className="lg:ml-[9em] ml-[3em] "> "I craft digital experiences that balance </span>
