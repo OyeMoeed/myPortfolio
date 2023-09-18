707593995c074fb00c74edb24b4b47e564aeb86a
Moeed Sarwar <oyemoeed@gmail.com>
2023-09-18 05:01:06 +0500

    Update Aboutpage.jsx

--- src/pages/About/Aboutpage.jsx
+++ src/pages/About/Aboutpage.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HeroAbout from "./HeroAbout";
 import Aim from "./Aim";
+import { motion, useAnimation } from "framer-motion";const Aboutpage = () => {
+   const controls = useAnimation();
 
-const Aboutpage = () => {
+   useEffect(() => {
+     controls.start({ y: 0, opacity: 1, transition: { duration: 1 } });
+   }, []);
   return (
-    <div>
-      <HeroAbout />
-      <Aim />
-    </div>
+    <motion.div initial={{ y: 200, opacity: 0 }} animate={controls}>
+      <div>
+        <HeroAbout />
+        <Aim />
+      </div>
+    </motion.div>
   );
 };
 
